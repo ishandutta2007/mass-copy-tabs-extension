fc1989515f326615e38aee963be95edaea134f02
ishan dutta <ishandutta2007@gmail.com>
2023-06-13 17:10:49 +0530

    popup

--- src/popup.tsx
+++ src/popup.tsx
@@ -8,9 +8,31 @@ import Todo from "components/Todo";
 import TodoContainer from "components/TodoContainer";
 import browser from "webextension-polyfill";
 
+const copy_titles_links = () => {
+  console.log("copy_titles_links");
+  // browser.runtime.sendMessage({ greeting: "getTabInfo" });
+}
+
+const copy_links = () => {
+  console.log("copy_links");
+  // browser.runtime.sendMessage({ greeting: "getTabInfo" });
+}
+
 const Popup = () => {
-  browser.runtime.sendMessage({ greeting: "getTabInfo" });
-  return (<div></div>);
+  return (<div>
+      <ul style="width:max-content; " >
+        <li id="actionCopy" style="display: block; ">
+          <div class="menu">
+            <button class="text" onClick={copy_titles_links}>Copy Titles and Links</button>
+          </div>
+        </li>
+        <li id="actionCopy" style="display: block; ">
+          <div class="menu">
+            <button class="text" onClick={copy_links}>Copy Links Only</button>
+          </div>
+        </li>
+      </ul>
+  </div>);
 };
 
 const root = document.createElement('div');
